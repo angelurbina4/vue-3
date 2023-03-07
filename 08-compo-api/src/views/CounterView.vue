7c3de38630e0942d905b0129ff9c24f240e9f86b
Angel Urbina <angelurbina44@gmail.com>
2023-03-07 12:41:11 -0300

    Added counter component

---
+++ 08-compo-api/src/views/CounterView.vue
@@ -0,0 +1,41 @@
+<template lang="">
+  <h1>Counter View</h1>
+  <h2>{{ counter }}</h2>
+
+  <button @click="increase">+1</button>
+  <button @click="decrease">-1</button>
+
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'CounterView',
+  props: {},
+  emits: [],
+  setup() {
+
+    const counter = ref(5)
+
+    // const increase = () => {
+    //   counter.value++
+    // }
+
+    // const decrease = () => {
+    //   counter.value--
+    // }
+
+    return {
+      counter,
+
+      decrease: () => counter.value--,
+      increase: () => counter.value++,
+    }
+  }
+}
+</script>
+
+<style lang="">
+  
+</style>
