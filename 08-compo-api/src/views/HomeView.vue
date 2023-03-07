a94ffa1d91cb5bc2042d1691a9295bd69b6a08fc
Angel Urbina <angelurbina44@gmail.com>
2023-03-07 16:13:13 -0300

    Added ref vs reactive

--- 08-compo-api/src/views/HomeView.vue
+++ 08-compo-api/src/views/HomeView.vue
@@ -1,12 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Reactive vs ref</h1>
+    <h3>{{ ironman }}</h3>
+    <h3>{{ hulk }}</h3>
+    <button @click="changeIronman">cambiar iron man</button>
+    <button @click="changeHulk">cambiar hulk</button>
+
+    <h3>{{ ironmanIsAlive }}</h3>
+    <h3>{{ hulkIsAlive }}</h3>
+    <button @click="changeStatus">cambiar status</button>
+
+
   </div>
 </template>
 
 <script>
+import { reactive, ref } from 'vue'
 
 export default {
   name: 'HomeView',
+  setup() {
+
+    const ironman = ref({ name: 'Tony Stark', age: 45 })
+    const hulk = reactive({ name: 'Bruce Banner', age: 35 })
+
+    const ironmanIsAlive = reactive(true) // reactive no funciona con tipos primitivos
+    const hulkIsAlive = ref(true) // ref funciona con tipos primitivos
+
+    const changeStatus = () => {
+      // ironmanIsAlive.isAlive = false // reactive no funciona con tipos primitivos
+      hulkIsAlive.value = false
+    }
+    
+    return {
+      ironman,
+      hulk,
+
+      ironmanIsAlive,
+      hulkIsAlive,
+      changeStatus,
+
+      changeIronman: () => {
+        ironman.value.name = 'Tony',
+        ironman.value.age = 'Stark'
+      },
+      changeHulk: () => {
+        hulk.name = 'Bruce',
+        hulk.age = 'Banner'
+      }
+    }
+
+  }
 }
 </script>
