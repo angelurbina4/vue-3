9fcbc454a9d58fa1c86d08c041e24b4894a16de8
Angel Urbina <angelurbina44@gmail.com>
2023-03-08 17:45:11 -0300

    Added watch to pokemon view

--- 08-compo-api/src/views/PokemonView.vue
+++ 08-compo-api/src/views/PokemonView.vue
@@ -1,18 +1,39 @@
 <template lang="">
-  <h1>Pokemon</h1>
+  <h1 v-if="!pokemon && !errorMessage">Buscando...</h1>
+  <h1 v-else-if="errorMessage">{{ errorMessage }}</h1>
+  <template v-else>
+    <h3>{{ pokemon.name }}</h3>
+    <img :src="pokemon.sprites.front_default" :alt="pokemon.name" />
+    <br/>
+    <router-link :to="{ name: 'pokemon-search'}">Regresar</router-link>
+  </template>
 </template>
 <script>
 import { useRoute } from 'vue-router'
+import usePokemon  from '../composables/usePokemon'
+import { watch } from 'vue'
 
 export default {
 
   setup() {
 
+    
     const route = useRoute()
+    
+    // console.log(route.params.id)
+    const { errorMessage, isLoading, pokemon, searchPokemon } = usePokemon( route.params.id )
 
-    console.log(route.params)
+    watch(
+      () => route.params.id,
+      () => searchPokemon(route.params.id)
+    )
 
-    return {}
+
+    return {
+      errorMessage,
+      isLoading,
+      pokemon,
+    }
   }
   
 }
